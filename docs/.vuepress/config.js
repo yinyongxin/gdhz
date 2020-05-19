b1d8c45636cdc7a9bbc987cd185ea161509a475c
孤独患者 <1101278643@qq.com>
2020-05-19 16:08:50 +0800

    'first'

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  base: '/docs/',
+  title: 'GDHZ',
+  description: '孤独患者的个人博客',
+  themeConfig: {
+    nav: [
+      { text: '首页',link: '/' },
+      { text: '关于',link: '/about' }
+    ]
+  }
+}
